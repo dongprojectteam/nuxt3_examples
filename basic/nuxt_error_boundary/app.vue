5e9e24d6b2dc940bdaaa35da6bb6d4e4de85aa19
Donghyuk Kim <donghyuk80.kim@gmail.com>
2023-09-14 10:46:05 +0900

    NuxtErrorBoundary Example

---
+++ basic/nuxt_error_boundary/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtErrorBoundary @error='errorLogger'>
+      <IAmErrorComponent></IAmErrorComponent>
+      <template #error='{ error, clearError }'>
+        <h1>에러 발생 시 이 메시지가 표시됩니다.</h1>
+        <button @click='clearError'>에러 지우기</button>
+        <hr />
+        {{ error }}
+      </template>
+    </NuxtErrorBoundary>
+  </div>
+</template>
+
+<script setup lang='ts'>
+const errorLogger = (error: unknown) => {
+  console.error('아래에 에러가 출력됩니다.')
+  console.log(error)
+}
+</script>
---
+++ basic/nuxt_error_boundary/components/IAmErrorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>원래 표시가 되어야 하는 메시지입니다.</div>
+</template>
+
+<script setup lang='ts'>
+const errorTriggered = useState<boolean>('errorTriggered', () => false)
+
+onMounted(() => {
+  if (!errorTriggered.value) {
+    errorTriggered.value = true
+    throw createError('Error Happened')
+  }
+})
+</script>
