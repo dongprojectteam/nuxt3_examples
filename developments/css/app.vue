3e08d3991137ef09a1096d8f84071124319263b7
Donghyuk Kim <donghyuk80.kim@gmail.com>
2023-09-22 22:32:35 +0900

    Add CSS Examples

---
+++ developments/css/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button>기본 버튼입니다</button>
+  </div>
+</template>
+
+<style>
+/* .(점)으로 시작하면 Class명을 의미합니다. */
+.flex {
+  display: flex;
+}
+
+/* #으로 시작하면 ID명을 의미합니다. */
+#id {
+  width: 100%;
+}
+
+/* tag명으로 만들 수 있습니다. */
+a {
+  color: blue;
+}
+</style>
