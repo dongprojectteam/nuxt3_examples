72e7fd6936da9dcb49271134eebba6502b1a32bb
Donghyuk Kim <donghyuk80.kim@gmail.com>
2023-09-23 11:47:32 +0900

    Change index.css directory

--- developments/css/nuxt.config.ts
+++ developments/css/nuxt.config.ts
@@ -1,4 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/index.css'],
+  css: ['~/assets/index.css', '~/assets/class_abbr.scss'],
 })
