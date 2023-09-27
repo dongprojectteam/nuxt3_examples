baa6b7d79e3fc4f2d64c2ea61207afd44319888e
Donghyuk Kim <donghyuk80.kim@gmail.com>
2023-09-27 22:04:47 +0900

    Lifecycle Code Update

--- vue3/vue3/src/components/Lifecycle.vue
+++ vue3/vue3/src/components/Lifecycle.vue
@@ -4,11 +4,12 @@ import Error from './Error.vue'
 
 const messages: string[] = reactive([])
 
+// 코드 1: 다양한 훅에 콜백함수 구현
 onBeforeMount(() => { messages.push('onBeforeMount') })
 onMounted(() => { messages.push('onMounted') })
 onBeforeUpdate(() => { messages.push('onBeforeUpdate') })
 
-// 코드 : 이 부분은 무한 업데이트에 빠집니다.
+// 코드 2: 이 부분은 무한 업데이트에 빠집니다.
 // onUpdated(() => { messages.push('onUpdated') })
 onErrorCaptured(() => { messages.push('onErrorCaptured') })
 </script>
