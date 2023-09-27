a88da35e804a76bbe547d73525dc75ddf3b69452
Donghyuk Kim <donghyuk80.kim@gmail.com>
2023-09-28 07:21:38 +0900

    타입스크립트 최초 파일 업로드

---
+++ typescript/basic.ts
@@ -0,0 +1,19 @@
+// 코드 1: variable에 number 값을 대입합니다.
+let variable = 1
+
+// 코드 2: 문자열 메소드 toUpperCase()를 호출합니다.
+let upperVariable = variable.toUpperCase()
+console.log(localeVariable)
+
+let stringVariable: string
+let numberVariable: number
+
+// 에러가 발생합니다.
+numberVariable = null
+// 에러가 발생합니다.
+stringVariable = undefined
+
+let stringOrNullVariable: string | null
+
+stringOrNullVariable = null
+stringOrNullVariable = 'string'
---
+++ typescript/javascript.js
@@ -0,0 +1,6 @@
+// 코드 1: variable에 number 값을 대입합니다.
+let variable = 1
+
+// 코드 2: 문자열 메소드 toUpperCase()를 호출합니다.
+let upperVariable = variable.toUpperCase()
+console.log(localeVariable)
