093aa9f353a71b697f4e140266e972cd8260adf8
Donghyuk Kim <donghyuk80.kim@gmail.com>
2023-11-01 08:01:58 +0900

    Checkbox Final Implementation

--- final/dmart/components/molecules/checkbox.vue
+++ final/dmart/components/molecules/checkbox.vue
@@ -1,28 +1,68 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 const props = defineProps({
   modelValue: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 })
 defineOptions({ inheritAttrs: false })
 
 const emits = defineEmits(['update:modelValue'])
 const onChange = (checked: boolean) => emits('update:modelValue', checked)
+
 const selected = ref(props.modelValue)
 
-const { id, type: _ } = useAttrs() as Record<string, string | boolean>
-const boxFormId = (id ?? `boxFormId-${Math.round(Math.random() * 10000).toString()}`) as string
+const {
+  id: originalId,
+  type: _,
+  ...attrs
+} = useAttrs() as Record<string, any>
+const id = (originalId ??
+  `boxFormId-${Math.round(Math.random() * 10000).toString()}`) as string
+const type = 'checkbox'
 
-watch(() => props.modelValue, newValue => selected.value = newValue)
+const fallThrough = reactive({
+  ...attrs,
+  id,
+  type,
+} as Record<string, any>)
+
+if (
+  attrs.checked != undefined &&
+  attrs.checked !== false &&
+  props.modelValue === false
+) {
+  onChange(true)
+}
+
+watch(
+  () => props.modelValue,
+  (newValue) => (selected.value = newValue)
+)
+
+onUpdated(() => {
+  console.log('updated')
+  Object.assign(fallThrough, {
+    ...useAttrs(),
+    id,
+    type,
+  })
+})
 </script>
 
 <template>
-  <span class='flex-vcenter' :class='useAttrs().class'>
-    <input type='checkbox' :id='boxFormId' v-bind='useAttrs()' v-model='selected'
-      @change.stop='onChange(($event.target as HTMLInputElement).checked)' />
-    <label v-if='useAttrs().label' :for='boxFormId'>
-      {{ useAttrs().label }}
+  <span class="vcenter" :class="useAttrs().class">
+    <input v-bind="fallThrough" v-model="selected"
+      @change.stop="onChange(($event.target as HTMLInputElement).checked)" />
+    <label :for="id" class='ml-1'>
+      <slot></slot>
     </label>
   </span>
 </template>
+
+<style scoped>
+.vcenter {
+  display: inline-flex;
+  align-items: center;
+}
+</style>
