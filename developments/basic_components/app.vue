cf51b3305340e33a5dea88301dc8e1d1687d83b8
Donghyuk Kim <donghyuk80.kim@gmail.com>
2023-11-01 08:05:24 +0900

    Finalize Checkbox

--- developments/basic_components/app.vue
+++ developments/basic_components/app.vue
@@ -1,35 +1,35 @@
-<script lang="ts" setup>
-  const smChecked = ref(false)
-  const mdChecked = ref(false)
-  const lgChecked = ref(false)
+<script lang='ts' setup>
+const smChecked = ref(false)
+const mdChecked = ref(false)
+const lgChecked = ref(false)
 </script>
 
 <template>
-  <div class="m-4">
-    <Input id="input_form" type="input" label="sm" class="sm" />
-    <Input id="input_form" type="input" label="md" class="md" />
-    <Input id="input_form" type="input" label="lg" class="lg" />
+  <div class='m-4'>
+    <Input id='input_form' type='input' label='sm' class='sm' />
+    <Input id='input_form' type='input' label='md' class='md' />
+    <Input id='input_form' type='input' label='lg' class='lg' />
 
-    <hr class="my-4" />
+    <hr class='my-4'>
 
     {{ `작은 크기의 체크박스 체크 여부: ${smChecked}` }}
-    <Checkbox
-      class="sm"
-      label="레이블"
-      v-model="smChecked"
-      name="smallCheckbox"
-    >
-    </Checkbox>
+    <div>
+      <Checkbox id='small_box' class='sm' v-model='smChecked'
+        name='smallCheckbox'>
+        레이블
+      </Checkbox>
+    </div>
     {{ `중간 크기의 체크박스 체크 여부: ${mdChecked}` }}
-    <Checkbox class="md" label="중간 크기" checked v-model="mdChecked">
-    </Checkbox>
+    <div>
+      <Checkbox class='md' checked v-model='mdChecked'>
+        중간 크기
+      </Checkbox>
+    </div>
     {{ `큰의 체크박스 체크 여부: ${lgChecked}` }}
-    <Checkbox
-      type="input"
-      class="lg t-primary"
-      label="큰 크기"
-      v-model="lgChecked"
-    >
-    </Checkbox>
+    <div>
+      <Checkbox type='input' class='lg t-primary' v-model='lgChecked'>
+        큰 크기
+      </Checkbox>
+    </div>
   </div>
 </template>
--- developments/basic_components/components/molecules/checkbox.vue
+++ developments/basic_components/components/molecules/checkbox.vue
@@ -1,64 +1,68 @@
 <script setup lang="ts">
-  const props = defineProps({
-    modelValue: {
-      type: Boolean,
-      default: false,
-    },
-  })
-  defineOptions({ inheritAttrs: false })
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+})
+defineOptions({ inheritAttrs: false })
 
-  const emits = defineEmits(['update:modelValue'])
-  const onChange = (checked: boolean) => emits('update:modelValue', checked)
+const emits = defineEmits(['update:modelValue'])
+const onChange = (checked: boolean) => emits('update:modelValue', checked)
 
-  const selected = ref(props.modelValue)
+const selected = ref(props.modelValue)
 
-  const {
-    id: originalId,
-    type: _,
-    ...attrs
-  } = useAttrs() as Record<string, any>
-  const id = (originalId ??
-    `boxFormId-${Math.round(Math.random() * 10000).toString()}`) as string
-  const type = 'checkbox'
+const {
+  id: originalId,
+  type: _,
+  ...attrs
+} = useAttrs() as Record<string, any>
+const id = (originalId ??
+  `boxFormId-${Math.round(Math.random() * 10000).toString()}`) as string
+const type = 'checkbox'
 
-  const fallThrough = reactive({
-    ...attrs,
+const fallThrough = reactive({
+  ...attrs,
+  id,
+  type,
+} as Record<string, any>)
+
+if (
+  attrs.checked != undefined &&
+  attrs.checked !== false &&
+  props.modelValue === false
+) {
+  onChange(true)
+}
+
+watch(
+  () => props.modelValue,
+  (newValue) => (selected.value = newValue)
+)
+
+onUpdated(() => {
+  console.log('updated')
+  Object.assign(fallThrough, {
+    ...useAttrs(),
     id,
     type,
-  } as Record<string, any>)
-
-  if (
-    attrs.checked != undefined &&
-    attrs.checked !== false &&
-    props.modelValue === false
-  ) {
-    onChange(true)
-  }
-
-  watch(
-    () => props.modelValue,
-    (newValue) => (selected.value = newValue)
-  )
-
-  onUpdated(() => {
-    console.log('updated')
-    Object.assign(fallThrough, {
-      ...useAttrs(),
-      id,
-      type,
-    })
   })
+})
 </script>
 
 <template>
-  <span class="flex-vcenter" :class="useAttrs().class">
-    <input
-      v-bind="fallThrough"
-      v-model="selected"
-      @change.stop="onChange(($event.target as HTMLInputElement).checked)"
-    />
-    <label v-if="useAttrs().label" :for="id">
-      {{ useAttrs().label }}
+  <span class="vcenter" :class="useAttrs().class">
+    <input v-bind="fallThrough" v-model="selected"
+      @change.stop="onChange(($event.target as HTMLInputElement).checked)" />
+    <label :for="id" class='ml-1'>
+      <slot></slot>
     </label>
   </span>
 </template>
+
+<style scoped>
+.vcenter {
+  display: inline-flex;
+  align-items: center;
+}
+</style>
